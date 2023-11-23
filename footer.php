3743272c5caf18cab7dcb7e17afd5852f3c44346
nahian28 <nahiansylhet@gmail.com>
2023-11-24 01:11:30 +0600

    Updates

---
+++ footer.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package kadence
+ */
+
+namespace Kadence;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$footer_about = get_field('footer_about', 'option');
+$footer_logo = $footer_about['footer_logo']['url'];
+$footer_description = $footer_about['footer_description'];
+$footer_menu_title = get_field('footer_menu_title', 'option');
+$footer_menu_title1 = $footer_menu_title['footer_menu_1_title'];
+$footer_menu_title2 = $footer_menu_title['footer_menu_2_title'];
+$footer_menu_title3 = $footer_menu_title['footer_menu_3_title'];
+$footer_menu_title4 = $footer_menu_title['footer_menu_4_title'];
+$footer_menu_title5 = $footer_menu_title['footer_menu_5_title'];
+?>
+
+</div><!-- #inner-wrap -->
+    <footer class="site-footer-area">
+        <div class="site-container">
+            <div class="footer">
+                <div class="footer-about">
+                    <?php 
+                        if($footer_logo) {
+                            ?>
+                                <img src="<?php echo esc_url($footer_logo); ?>" alt="">
+                            <?php
+                        }
+                    ?>
+                    <p><?php echo $footer_description;?></p>
+                </div>
+                <div class="single-footer">
+                    <?php 
+                        if($footer_menu_title1) {
+                            ?> <h4><?php echo $footer_menu_title1;?></h4> <?php
+                        }
+                    ?>                    
+                    <?php 
+                        if ( has_nav_menu( 'footer_1' ) ) {
+                            wp_nav_menu( array( 'theme_location' => 'footer_1' ) );
+                        }
+                    ?>
+                </div>
+                <div class="single-footer">
+                    <?php 
+                        if($footer_menu_title2) {
+                            ?> <h4><?php echo $footer_menu_title2;?></h4> <?php
+                        }
+                    ?>     
+                    <?php 
+                        if ( has_nav_menu( 'footer_2' ) ) {
+                            wp_nav_menu( array( 'theme_location' => 'footer_2' ) );
+                        }
+                    ?>
+                </div>
+                <div class="single-footer">
+                    <?php 
+                        if($footer_menu_title3) {
+                            ?> <h4><?php echo $footer_menu_title3;?></h4> <?php
+                        }
+                    ?>     
+                    <?php 
+                        if ( has_nav_menu( 'footer_3' ) ) {
+                            wp_nav_menu( array( 'theme_location' => 'footer_3' ) );
+                        }
+                    ?>
+                </div>
+                <div class="single-footer">
+                    <?php 
+                        if($footer_menu_title4) {
+                            ?> <h4><?php echo $footer_menu_title4;?></h4> <?php
+                        }
+                    ?>     
+                    <?php 
+                        if ( has_nav_menu( 'footer_4' ) ) {
+                            wp_nav_menu( array( 'theme_location' => 'footer_4' ) );
+                        }
+                    ?>
+                </div>
+                <div class="single-footer">
+                    <?php 
+                        if($footer_menu_title5) {
+                            ?> <h4><?php echo $footer_menu_title5;?></h4> <?php
+                        }
+                    ?>     
+                    <?php 
+                        if ( has_nav_menu( 'footer_5' ) ) {
+                            wp_nav_menu( array( 'theme_location' => 'footer_5' ) );
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <section class="footer-bottom-area">
+        <div class="site-container">
+            <div class="footer-bottom">
+                <p>Copyright © 2023 <a href="">Fluent Forms</a>. A Brand of <a href="">WPManageNinja™</a></p>
+                <?php 
+                    if ( has_nav_menu( 'footer_menu' ) ) {
+                        wp_nav_menu( array( 'theme_location' => 'footer_menu' ) );
+                    }
+                ?>
+            </div>
+        </div>
+    </section>
+</div><!-- #wrapper -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php
+
+if ( ! function_exists( 'kadence_child_register_nav_menu' ) ) {
+	function kadence_child_register_nav_menu(){
+		register_nav_menus( array(
+	    	'footer_1' => __( 'Footer 1', 'kadence' ),
+	    	'footer_2' => __( 'Footer 2', 'kadence' ),
+	    	'footer_3' => __( 'Footer 3', 'kadence' ),
+	    	'footer_4' => __( 'Footer 4', 'kadence' ),
+	    	'footer_5' => __( 'Footer 5', 'kadence' ),
+	    	'footer_menu' => __( 'Footer Menu', 'kadence' ),
+		) );
+	}
+	add_action( 'after_setup_theme', 'kadence_child_register_nav_menu');
+}
+
+
+add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_styles' );
+function mytheme_enqueue_styles() {
+    wp_enqueue_style( 'mytheme-parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'mytheme-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( 'mytheme-parent-style' ),
+        wp_get_theme()->get('Version')
+    );
+}
+
+/**
+ * Site Option Page 
+*/
+
+if( function_exists('acf_add_options_page') ) {	
+    acf_add_options_page(array(
+      'page_title' 	=> 'Site Settings',
+      'menu_title'	=> 'Site Settings',
+      'menu_slug' 	=> 'fluentforms-settings',
+      'capability' => 'edit_posts',
+      'icon_url'   => 'dashicons-welcome-widgets-menus',
+      'position'   => 2,
+      'redirect'		=> false
+    ));    
+  }
